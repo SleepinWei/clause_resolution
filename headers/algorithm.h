dc43d327ad1d4d34fa06daf3e464ac26eb700727
SleepinWei <zyw0241@outlook.com>
2022-05-15 16:51:12 +0800

    add unify function

---
+++ headers/algorithm.h
@@ -0,0 +1,11 @@
+#include"clause.h"
+#include"knowlegebase.h"
+#include<map>
+bool fUnifyPred(Predicate &x, Predicate &y, std::map<std::string, std::string> &mapSub);
+bool fUnifyLiteral(
+    Literal &x, Literal &y,
+    std::map<std::string, std::string> &mapSub);
+bool fUnifyCNF(
+    CNF &x, CNF &y,
+    std::map<std::string, std::string> &mapSub);
+bool fUnifyDNF(DNF &x, DNF &y, std::map<std::string, std::string> &mapSub);
